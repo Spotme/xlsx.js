30afb4e94ba0f3484e63876e2fb39d7310af615b
Benoit Baliguet <benoit@spotme.com>
2021-03-09 18:31:35 +0100

    fix: updated tests and dev dependencies

--- test/unitary.js
+++ test/unitary.js
@@ -3,12 +3,12 @@ var xlsx = require('../xlsx');
 var fs = require('fs-extra');
 var path = require('path');
 
-describe('XlsX.js unit tests', function() {
+describe('XlsX.js unit tests', function () {
 
   var file = path.join('test', 'output', 'inflate-1.xlsx');
-  
-  before(function(done) {
-    fs.remove(file, function(err) {
+
+  before(function (done) {
+    fs.remove(file, function (err) {
       if (err) {
         return done(err);
       }
@@ -16,35 +16,35 @@ describe('XlsX.js unit tests', function() {
     });
   })
 
-  it('should simple xlsx be written (you can manually check '+file+')', function(done) {
+  it('should simple xlsx be written (you can manually check ' + file + ')', function (done) {
     var sheet = xlsx({
       creator: 'John Doe',
       lastModifiedBy: 'Meg White',
       worksheets: [{
         data: [
-          ['green', 'white', {value:'orange', autoWidth:true}, 'blue', 'red'],
+          ['green', 'white', { value: 'orange', autoWidth: true }, 'blue', 'red'],
           ['1', '2', '3', '4', '5'],
           [6, 7, 8, 9, 10]
         ],
         table: true,
         name: 'Sheet 1'
-      },{
+      }, {
         data: [
           ['formatting test'],
-          [{formatCode: '0.00', value:'1'}, 
-           {italic:1, bold:1, hAlign:'center', borders:{bottom:'DEE31D'}, value:'B1'}, 
-           {borders:{bottom:64}, value:'C1'},
-           {fontName: 'Arial', value:'D1'},
-           {fontSize: 8, value:'E1'}, 
-           {italic:1, bold:1, value: 'F1'}
+          [{ formatCode: '0.00', value: '1' },
+          { italic: 1, bold: 1, hAlign: 'center', borders: { bottom: 'DEE31D' }, value: 'B1' },
+          { borders: { bottom: 64 }, value: 'C1' },
+          { fontName: 'Arial', value: 'D1' },
+          { fontSize: 8, value: 'E1' },
+          { italic: 1, bold: 1, value: 'F1' }
           ]
         ],
         name: 'Sheet 2'
-      },{
+      }, {
         data: [
           ['merge test'],
-          ['A1', {colSpan:3, value:'B1'}, 'E1'],
-          [{rowSpan: 3, value:'A2', vAlign: 'center', hAlign: 'center'}, 'B2', 'C2', 'D2', 'E2'],
+          ['A1', { colSpan: 3, value: 'B1' }, 'E1'],
+          [{ rowSpan: 3, value: 'A2', vAlign: 'center', hAlign: 'center' }, 'B2', 'C2', 'D2', 'E2'],
           ['B3', 'C3', 'D3', 'E3']
         ],
         name: 'Sheet 3'
@@ -53,8 +53,8 @@ describe('XlsX.js unit tests', function() {
     fs.writeFile(file, sheet.base64, 'base64', done);
   })
 
-  it('should generated xlsx be readable', function(done) {
-    fs.readFile(file, 'base64', function(err, content) {
+  it('should generated xlsx be readable', function (done) {
+    fs.readFile(file, 'base64', function (err, content) {
       if (err) {
         return done(err);
       }
@@ -65,23 +65,23 @@ describe('XlsX.js unit tests', function() {
         name: 'Sheet 1',
         data: [
           [
-            {value: 'green', formatCode: 'General'},
-            {value: 'white', formatCode: 'General'},
-            {value: 'orange', formatCode: 'General'},
-            {value: 'blue', formatCode: 'General'},
-            {value: 'red', formatCode: 'General'}
-          ],[ 
-            {value: 1, formatCode: 'General'},
-            {value: 2, formatCode: 'General'},
-            {value: 3, formatCode: 'General'},
-            {value: 4, formatCode: 'General'},
-            {value: 5, formatCode: 'General'}
-          ],[
-            {value: 6, formatCode: 'General'},
-            {value: 7, formatCode: 'General'},
-            {value: 8, formatCode: 'General'},
-            {value: 9, formatCode: 'General'},
-            {value: 10, formatCode: 'General'}
+            { value: 'green', formatCode: 'General' },
+            { value: 'white', formatCode: 'General' },
+            { value: 'orange', formatCode: 'General' },
+            { value: 'blue', formatCode: 'General' },
+            { value: 'red', formatCode: 'General' }
+          ], [
+            { value: '1', formatCode: 'General' },
+            { value: '2', formatCode: 'General' },
+            { value: '3', formatCode: 'General' },
+            { value: '4', formatCode: 'General' },
+            { value: '5', formatCode: 'General' }
+          ], [
+            { value: 6, formatCode: 'General' },
+            { value: 7, formatCode: 'General' },
+            { value: 8, formatCode: 'General' },
+            { value: 9, formatCode: 'General' },
+            { value: 10, formatCode: 'General' }
           ]
         ],
         table: false,
@@ -91,14 +91,14 @@ describe('XlsX.js unit tests', function() {
       assert.deepEqual(sheet.worksheets[1], {
         name: 'Sheet 2',
         data: [
-          [{value: 'formatting test', formatCode: 'General'}],
+          [{ value: 'formatting test', formatCode: 'General' }],
           [
-            {value: 1, formatCode: '0.00'},
-            {value: 'B1', formatCode: 'General'},
-            {value: 'C1', formatCode: 'General'},
-            {value: 'D1', formatCode: 'General'},
-            {value: 'E1', formatCode: 'General'},
-            {value: 'F1', formatCode: 'General'}
+            { value: '1', formatCode: '0.00' },
+            { value: 'B1', formatCode: 'General' },
+            { value: 'C1', formatCode: 'General' },
+            { value: 'D1', formatCode: 'General' },
+            { value: 'E1', formatCode: 'General' },
+            { value: 'F1', formatCode: 'General' }
           ]
         ],
         table: false,
@@ -108,25 +108,25 @@ describe('XlsX.js unit tests', function() {
       assert.equal(JSON.stringify(sheet.worksheets[2]), JSON.stringify({
         name: 'Sheet 3',
         data: [
-          [{value: 'merge test', formatCode: 'General'}],
+          [{ value: 'merge test', formatCode: 'General' }],
           [
-            {value: 'A1', formatCode: 'General'},
-            {value: 'B1', formatCode: 'General'},
-            {value: null, formatCode: 'General'},
-            {value: null, formatCode: 'General'},
-            {value: 'E1', formatCode: 'General'}
-          ],[
-            {value: 'A2', formatCode: 'General'},
-            {value: 'B2', formatCode: 'General'},
-            {value: 'C2', formatCode: 'General'},
-            {value: 'D2', formatCode: 'General'},
-            {value: 'E2', formatCode: 'General'}
-          ],[
-            {value: null, formatCode: 'General'},
-            {value: 'B3', formatCode: 'General'},
-            {value: 'C3', formatCode: 'General'},
-            {value: 'D3', formatCode: 'General'},
-            {value: 'E3', formatCode: 'General'}
+            { value: 'A1', formatCode: 'General' },
+            { value: 'B1', formatCode: 'General' },
+            { value: null, formatCode: 'General' },
+            { value: null, formatCode: 'General' },
+            { value: 'E1', formatCode: 'General' }
+          ], [
+            { value: 'A2', formatCode: 'General' },
+            { value: 'B2', formatCode: 'General' },
+            { value: 'C2', formatCode: 'General' },
+            { value: 'D2', formatCode: 'General' },
+            { value: 'E2', formatCode: 'General' }
+          ], [
+            { value: null, formatCode: 'General' },
+            { value: 'B3', formatCode: 'General' },
+            { value: 'C3', formatCode: 'General' },
+            { value: 'D3', formatCode: 'General' },
+            { value: 'E3', formatCode: 'General' }
           ]
         ],
         table: false,
